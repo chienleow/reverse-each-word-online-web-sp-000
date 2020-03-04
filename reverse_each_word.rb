cc4a06462f553be0fddf89b579260c04457e7063
C. Leow <chienleow1@gmail.com>
2020-03-04 05:13:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(sentence1)
-  reversed_array = sentence1.split(" ").map do |word|
+  reversed_array = sentence1.split(" ").collect do |word|
     word.reverse
   end
   reversed_array.join(" ")
